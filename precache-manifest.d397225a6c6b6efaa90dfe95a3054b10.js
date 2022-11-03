d4f1061df83af296d07bbe3a39b8f7e4c51b0788
abigaeldev <traderbotx@protonmail.com>
2022-11-03 17:12:44 +0700

    initial commit

---
+++ precache-manifest.d397225a6c6b6efaa90dfe95a3054b10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3bb69c2f1c645509eb968e3cbbfb8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9ef5f543d29e699a15",
+    "url": "/static/css/2.b5373d28.chunk.css"
+  },
+  {
+    "revision": "20473cd1ec395e08ea49",
+    "url": "/static/css/main.a7d078b1.chunk.css"
+  },
+  {
+    "revision": "0f9ef5f543d29e699a15",
+    "url": "/static/js/2.979fdc9b.chunk.js"
+  },
+  {
+    "revision": "f17da6b607f5fed6a3cf5150f134fee5",
+    "url": "/static/js/2.979fdc9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20473cd1ec395e08ea49",
+    "url": "/static/js/main.6bc4da3c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6bc4da3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a60ba7f7f55f86ad37b",
+    "url": "/static/js/runtime-main.5f218360.js"
+  }
+]);
